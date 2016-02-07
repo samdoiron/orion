0b95c442d7498eec014efaf01e1c347e3b458482
Samuel Doiron <sam.doiron96@gmail.com>
2016-02-07 17:40:02 -0400

    Remove use of deprecated thread::sleep_ms

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,12 @@
 
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 pub fn ms_timer(ms: u32) -> mpsc::Receiver<()> {
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || {
-        thread::sleep_ms(ms);
+        thread::sleep(Duration::from_millis(ms as u64));
         tx.send(()).unwrap();
     });
     rx
